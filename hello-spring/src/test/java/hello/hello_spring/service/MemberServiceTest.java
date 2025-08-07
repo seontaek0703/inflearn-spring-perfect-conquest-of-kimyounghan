b0a40431dd026c1606c639fbb5f4f1c47d4ca20a
Seontaek Lim <seontaek1998@gmail.com>
2025-08-07 14:42:44 +0900

    Member Repository and Member Service and Test Code

---
+++ hello-spring/src/test/java/hello/hello_spring/service/MemberServiceTest.java
@@ -0,0 +1,47 @@
+package hello.hello_spring.service;
+
+import hello.hello_spring.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    MemberService memberService = new MemberService();
+
+    @Test
+    void 회원가입() {
+        // given
+        Member member = new Member();
+        member.setName("hello");
+
+        // when
+        Long saveId = memberService.join(member);
+
+        // then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void 중복_회원_예외() {
+        // given
+
+        // when
+
+        // then
+        
+    }
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
