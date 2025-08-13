01c469ad6469da251d9a6264ce543d95322f8e9c
Seontaek Lim <seontaek1998@gmail.com>
2025-08-13 11:43:58 +0900

    ADD: 자바로 직접 스프링 빈 등록하기

--- hello-spring/src/test/java/hello/hello_spring/service/MemberServiceTest.java
+++ hello-spring/src/test/java/hello/hello_spring/service/MemberServiceTest.java
@@ -1,28 +1,40 @@
 package hello.hello_spring.service;
 
 import hello.hello_spring.domain.Member;
-import org.assertj.core.api.Assertions;
+import hello.hello_spring.repository.MemberRepository;
+import hello.hello_spring.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.util.Optional;
-
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class MemberServiceTest {
 
-    MemberService memberService = new MemberService();
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
+
+    @BeforeEach
+    public void beforeEach() {
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
+
+    @AfterEach
+    public void afterEach() {
+        memberRepository.clearStore();
+    }
 
     @Test
     void 회원가입() {
-        // given
+        // given : 상황이 주어짐
         Member member = new Member();
-        member.setName("hello");
+        member.setName("spring");
 
-        // when
+        // when : 어떤 조치를 취함
         Long saveId = memberService.join(member);
 
-        // then
+        // then : 어떤 결과가 나옴
         Member findMember = memberService.findOne(saveId).get();
         assertThat(member.getName()).isEqualTo(findMember.getName());
     }
@@ -30,11 +42,28 @@ class MemberServiceTest {
     @Test
     public void 중복_회원_예외() {
         // given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
 
         // when
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class,
+                () -> memberService.join(member2));
+
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+
+//        try {
+//            memberService.join(member2);
+//            fail();
+//        } catch (IllegalStateException e) {
+//            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.123123");
+//        }
 
         // then
-        
+
     }
 
     @Test
@@ -44,4 +73,5 @@ class MemberServiceTest {
     @Test
     void findOne() {
     }
+
 }
